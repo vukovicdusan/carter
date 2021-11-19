7dfe5a330e88f64fa3f3cc4a5de2cf26f947f7ed
unknown <picvajzmen@gmail.com>
2021-11-19 16:40:14 +0100

    poslednje promene

--- carter_dow-desktop/js/form-validation.js
+++ carter_dow-desktop/js/form-validation.js
@@ -1,6 +1,8 @@
 const email = document.getElementById("email");
 const firstName = document.getElementById("first__name");
 const btn = document.getElementById("form__button");
+const form = document.getElementById("form");
+
 
 form.addEventListener('input', () => {
     checkIfEmpty();
@@ -21,14 +23,38 @@ form.addEventListener('submit', e => {
     checkInput();
 });
 
+let thankYou = document.querySelector(".thank__you--message");
+let imIn = document.querySelector(".contact__form--button");
+
 function checkInput() {
-    const emailValue = email.value.trim();
+    let emailValue = email.value.trim();
     let checkMailValue = checkMail(emailValue);
     if(checkMailValue === false) {
+        let valueSave = email.value;
         email.value = "";
         email.placeholder = "Invalid email";
         email.classList.add('error');
+        setTimeout(()=>{
+            email.value = valueSave;
+            // email.placeholder = "";
+            email.classList.remove('error');
+        }, 3000)
+    } else {
+        thankYou.classList.add("show");
+        imIn.classList.add("no-show");
+        form.reset();
+        setTimeout(()=>{
+            thankYou.classList.remove("show");
+            
+            setTimeout(()=>{
+                imIn.classList.remove("no-show");
+                btn.disabled = true;
+            }, 500)
+           
+        }, 3000)
+        
     }
+    
 }
 
 function checkMail(input){
